6f9a91e7a4d5966da3d7fb21f5269f6e1da3c9db
Jerry YT Chen <nmjk2000@gmail.com>
2020-10-17 12:29:36 +0800

    test: Add debug log

--- app/api/tap_pay/echo.py
+++ app/api/tap_pay/echo.py
@@ -1,4 +1,5 @@
 from sanic import Blueprint
+from sanic.log import logger
 from sanic.response import json
 
 bp = Blueprint("tap_pay", url_prefix="")
@@ -6,6 +7,16 @@ bp = Blueprint("tap_pay", url_prefix="")
 
 @bp.route("/echo", methods=["GET"])
 async def echo(request):
+    logger.info(
+        json.dumps(
+            {
+                "parsed": True,
+                "args": request.args,
+                "url": request.url,
+                "query_string": request.query_string,
+            }
+        )
+    )
     return json(
         {
             "parsed": True,
